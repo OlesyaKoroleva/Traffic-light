dc72ecf440792ae76f7b5156f16044908e9d4dc8
Olesya <favourite89@yandex.ru>
2021-08-18 19:30:01 +0300

    Setup corner Radius

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 75
+        yellowView.layer.cornerRadius = 75
+        greenView.layer.cornerRadius = 75
+        
+    
+        
     }
 
 
